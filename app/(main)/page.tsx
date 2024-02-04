d1cf386acbabcff94b46c1eec8da15c64a445ea9
ngdvan90 <ngdvan@gmail.com>
2024-02-04 22:36:11 +0700

    Init commit

---
+++ app/(main)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-red-400">Protected Route</h1>
+      <Button>Test</Button>
+    </div>
+  );
+}
